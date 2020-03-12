2232247f8de499044abdd6a923edeb913428db24
alireza <alireza.jvh98@gmail.com>
2020-03-12 14:51:19 +0330

    set timeline api

--- src/app/admin/library/timeline/timeline.component.ts
+++ src/app/admin/library/timeline/timeline.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
+import * as moment from 'jalali-moment';
 
 @Component({
   selector: 'app-timeline-component',
@@ -12,6 +13,7 @@ export class TimelineComponent implements OnInit {
 
   loading = false;
   productSizeId;
+  timeline: any;
 
   constructor(private http: HttpClient,
               private route: ActivatedRoute) {
@@ -22,13 +24,22 @@ export class TimelineComponent implements OnInit {
     this.getData();
   }
 
+  showJalaliDate(date) {
+    // console.log(date)
+    moment.locale('fa', { useGregorianParser: true });
+    date = moment(date).format();
+    let m = moment.from(date, 'fa', 'YYYY/MM/DDTHH:mmZ');
+    console.log(m)
+    // console.log(m.local('fa').format('hh:mm')
+    return m.local('fa').format('jYYYY/jMM/jDD hh:mm');
+  }
+
   getData() {
     this.loading = true;
     const param = {
-      productSize: {
-        id: this.productSizeId
-      }
+      id: this.productSizeId
     };
+    console.log(param);
     this.http.post('http://127.0.0.1:9000/v1/shop/timeline/list', param, {
       headers: {
         Authorization: 'Bearer ' + localStorage.getItem('token')
@@ -37,6 +48,8 @@ export class TimelineComponent implements OnInit {
       .subscribe(
         val => {
           console.log(val);
+          this.loading = false;
+          this.timeline = val
         }
       ),
       err => {
