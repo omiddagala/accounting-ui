290f5f7dbd3bd3f78643f675c158f31714e955bb
alireza <alireza.jvh98@gmail.com>
2020-03-12 05:41:51 +0330

    update

--- src/app/admin/library/timeline/timeline.component.ts
+++ src/app/admin/library/timeline/timeline.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-timeline-component',
@@ -6,6 +8,39 @@ import {Component} from '@angular/core';
   styleUrls: ['./timeline.component.scss']
 })
 
-export class TimelineComponent {
+export class TimelineComponent implements OnInit {
 
+  loading = false;
+  productSizeId;
+
+  constructor(private http: HttpClient,
+              private route: ActivatedRoute) {
+    this.productSizeId = route.snapshot.params.id;
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.loading = true;
+    const param = {
+      productSize: {
+        id: this.productSizeId
+      }
+    };
+    this.http.post('http://127.0.0.1:9000/v1/shop/timeline/list', param, {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    })
+      .subscribe(
+        val => {
+          console.log(val);
+        }
+      ),
+      err => {
+        console.log(err);
+      };
+  }
 }
